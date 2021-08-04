cd3a2eeff376c914da2ed330fcaf77e76390df6e
ShimadzuYo <55920277+ShimadzuYo@users.noreply.github.com>
2021-08-04 11:13:26 +0600

    Кажись получилось 3.0

---
+++ Task3/SubTask8Real.cs
@@ -0,0 +1,35 @@
+﻿using Utilities;
+using System;
+using System.Collections.Generic;
+
+
+namespace Task3
+{
+    public class SubTask8Real: BaseTask
+    {
+        public override void Start()
+        {
+            const string ageDefault = "1";
+            var theResult = new List<int>();
+            Console.WriteLine("How old in days are you my man/woman?");
+            var theAge = int.Parse(Console.ReadLine() ?? ageDefault);
+            const int daysInYear = 365;
+            const int daysInMonth = 30;
+            var ageInYears = Math.DivRem(theAge,daysInYear,out var remainderMonths);
+            var ageInMonths = Math.DivRem(remainderMonths,daysInMonth, out var remainderDays);
+            var ageInDays = remainderDays;
+            theResult.Add(ageInYears);
+            theResult.Add(ageInMonths);
+            theResult.Add(ageInDays);
+            Console.WriteLine($"Years: {theResult[0]}; Months: {theResult[1]}; Days: {theResult[2]};");
+
+        }
+
+       
+        private static int CalculateMonths(int age)
+        {
+            var ageInMonths = age / 30;
+            return ageInMonths;
+        }
+    }
+}
