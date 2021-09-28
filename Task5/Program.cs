ead6c7801c6b9adb21c222eaa0847f459ccd502c
ShimadzuYo <55920277+ShimadzuYo@users.noreply.github.com>
2021-09-28 15:55:43 +0600

    Task 5

---
+++ Task5/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Utilities;
+
+
+namespace Task5
+{
+    public class Program 
+
+    {
+        private static void Main(string[] args)
+        {
+            var SubTask2 = new SubTask2();
+            SubTask2.Start();
+        }
+    }
+}
