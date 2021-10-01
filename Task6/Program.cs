de0c439c73253ae535fb255bd4546f4aefe45e97
ShimadzuYo <55920277+ShimadzuYo@users.noreply.github.com>
2021-10-01 21:45:45 +0600

    Task 6 first 2 SubTasks

---
+++ Task6/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+
+namespace Task6
+{
+    class Program
+    {
+        private static void Main(string[] args)
+        {
+            var SubTask2 = new SubTask2();
+            SubTask2.Start();
+
+        }
+    }
+}
---
+++ Task6/SubTask2.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Utilities;
+
+namespace Task6
+{
+    public class SubTask2:BaseTask
+    
+    {
+        public override void Start()
+        {
+            base.Start();
+            Console.WriteLine("Hi there retard!");
+            var result = TheJob();
+            Console.WriteLine(result);
+            
+        }
+
+        private static int TheJob()
+        {
+            var myInts = new List<int>(10);
+            for (var i = 0; i < myInts.Capacity; i++)
+            {
+                Console.WriteLine("Enter a value pls!");
+                int theElement = Int32.Parse(Console.ReadLine() ?? string.Empty);
+                myInts.Add(theElement);
+            }
+
+            var theResult = 0;
+            string op = "";
+            Console.WriteLine("Enter the operation pls - avg, min or max");
+            var theOpChoice = Console.ReadLine();
+            if (theOpChoice == "avg")
+            {
+                theResult = myInts.Sum() / myInts.Count;
+            }
+
+            if (theOpChoice == "min")
+            {
+                theResult = myInts.Min();
+            }
+
+            if (theOpChoice == "max")
+            {
+                theResult = myInts.Max();
+            }
+
+            return theResult;
+
+
+        }
+    }
+}
