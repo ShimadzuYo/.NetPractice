9c3e696a2d7d1f97db9948ae1278c657d653dc27
vladimir.nedoroslev@gmail.com <UT73G4bEwBiJcXG>
2021-12-27 01:25:58 +0600

    Added comments to original oop project;

--- OopPractice/Group.cs
+++ OopPractice/Group.cs
@@ -11,18 +11,54 @@ namespace OopPractice
         public List<Student> GraduatedWithHonorsStudents = new();
         public List<Student> Failed = new();
         public int Number;
+        // I'm not sure about this, but doesn't school dictate the marks thresholds?
+        // I think group should not care about which students in group are good or bad
+        // I see group as just a container that aggregates students and provide some logic 
+        // to add/remove students
+        // And then school will make a decision about a group because it defines standards for graduation
+        // But that depends on your perspective and I might be wrong
         public int HonorsMark = 400;
         public int GraduationMark = 300;
         public string Time;
         public string Level;
 
 
+        // FYI
+        // Good practice to avoid 'bad' object initialization on the object creation, i.e. in Constructor
+        // This will avoid having bad objects to spread in your program
+        // You can do it the following way
+        // public Group(string level, string time)
+        // {
+        //     if (string.IsNullOrWhiteSpace(level))
+        //         throw new ArgumentNullException(nameof(level), "Level cannot be empty!");
+        //     Level = level; 
+        //     if (string.IsNullOrWhiteSpace(time))
+        //         throw new ArgumentNullException(nameof(time), "Time cannot be empty!");
+        //     Time = time;
+        // }
+        // This way you are sure, that your program will always have valid 'group' objects
+        // In case there is an attempt of creating a 'bad' group (like with level = null or '')
+        // your program will immediately report this with exception mechanism
+        // this may prevent lots of different bugs and errors
+        // It is good because you will be sure that whenever you are dealing with any 'Group' object 
+        // it is in valid state and will not have side effects
+        // e.g. imagine if you you had a group with level=null and somewhere else would try to do something with this
+        // like capitalize it (level.ToUpper()) - this would throw NulLReferenceException 
+        // since level is 'null' and 'null' does not have 'ToUpper()' method
         public Group(string level, string time)
         {
             Level = level;
             Time = time;
         }
 
+        // Ideally, group should not be aware of how a student is created and be responsible to create it
+        // Better to have
+        // public void AddStudent(Student student)
+        // {
+        //     Class.Add(student);
+        // }
+        // But that's ok here, since the method name reflects its intention 
+        
         public void CreateAndAddStudent()
         {
             while (true)
@@ -46,6 +82,7 @@ namespace OopPractice
             }
         }
 
+        // good
         public void PrintInfo()
         {
             foreach (var stud in Class)
@@ -60,8 +97,15 @@ namespace OopPractice
             }
         }
 
+        // Better to name it RemoveStudentByName
+        // Good
         public void RemoveStudent(string name)
         {
+            // FYI
+            // Could be rewritten like Class.RemoveAll(student => student.name == name); 
+            // but that would remove all students with the same name, so your solution is better in that sense
+            // However, if you have 2 "John's", this method will remove only "John" who appears first
+            // This is already another problem and up to you how to build this logic.
             foreach (var student in Class)
             {
                 if (name == student.name)
@@ -76,6 +120,9 @@ namespace OopPractice
         public void OutputResults()
         {
             Console.WriteLine("Honorary graduates:");
+            // this for each loop is repeated 3 times, you could utilize the fact
+            // that all 3 lists contain 'Student's and create a helper method to output 
+            // students names and marks (see OutputResultsRefactored & PrintStudents below)
             foreach (var honor in GraduatedWithHonorsStudents)
             {
                 Console.WriteLine($"{honor.name} {honor.marks}");
@@ -90,6 +137,26 @@ namespace OopPractice
             foreach (var retard in Failed)
             {
                 Console.WriteLine($"{retard.name} {retard.marks}");
+            }
+        } 
+        
+        public void OutputResultsRefactored()
+        {
+            Console.WriteLine("Honorary graduates:");
+            PrintStudents(GraduatedWithHonorsStudents);
+
+            Console.WriteLine("Regular dummies:");
+            PrintStudents(GraduatedStudents);
+            
+            Console.WriteLine("The actual dumbasses:");
+            PrintStudents(Failed);
+        }
+
+        private void PrintStudents(List<Student> students)
+        {
+            foreach (var student in students)
+            {
+                Console.WriteLine($"{student.name} {student.marks}");
             }
         }
 
--- OopPractice/School.cs
+++ OopPractice/School.cs
@@ -5,10 +5,17 @@ namespace OopPractice
 {
     public class School
     {
+        // better to make it private, because you are usually exposing some methods to work with group
+        // and do not allow user to directly access internals of the 'School'
+        // Usually you expose some methods that introduce logic to make sure your 'School' is always in a valid state
+        // E.g. 'public' in this case allows someone to write School.GroupList = new List<Student>()
+        // that would basically 'break' the school
+        // to avoid that you do not usually expose internals of your school by making fields 'private'
         public static List<Group> GroupList = new();
         public static int Capacity = 10;
 
-        protected static readonly List<string> Levels = new()
+        // You can expose this field and make it public
+        public static readonly List<string> Levels = new()
         {
             "beginner", "elementary", "pre-intermediate", "intermediate"
         };
@@ -29,6 +36,7 @@ namespace OopPractice
                 var timeInput = (Console.ReadLine());
                 GroupList.Add(new Group(level, timeInput));
                 Console.WriteLine("Would you like to continue Y or N?");
+                // good point - ToUpper().Trim() - always good to 'normalize' user input 
                 var answer = Console.ReadLine()?.ToUpper().Trim();
                 if (answer == "N")
                 {
@@ -65,14 +73,32 @@ namespace OopPractice
             }
         }
 
+        // FYI
+        // Here, it sounds generally logical like method on School that allows to create some random groups inside of it
+        // However, in programming it is better to always divide components have as less responsibilities as possible
+        // Like in this case as I understood. A school object encapsulates and aggregates groups of students and exposes a user
+        // some methods to manipulate those groups
+        // There is a pattern in programming called 'Factory' that you could utilize here
+        // But that's also questionable and purely depends on your domain, the program, and how it is going to be used
         public static void GenerateRandomGroups()
         {
             var level = "";
             var time = "";
             for (var i = 0; i < Capacity; i++)
             {
+                // Random object is generated on each loop iteration, better to move it outside the loop
+                // so it is created once
                 var rand = new Random();
                 var die = rand.Next(0, Levels.Count);
+                // FYI
+                // if you have already 'i' name in use, better to use j and k, it's like a convention in nested loops
+                // i, j, k, n, m
+                // Or you could go the other way and name it meaningful
+                // like here you are traversing Levels, so you can do for (var level = 0; level < Levels.Count; level)
+                
+                // Moreover, feels like you do not even need these loops here
+                // Because when you roll a die you already have some random number
+                //
                 for (var a = 0; a < Levels.Count; a++)
                 {
                     if (a == die)
@@ -97,6 +123,9 @@ namespace OopPractice
 
         public static void WorkWithGroup(int num)
         {
+            // since you are already given a num of a group you do not need traverse the groups in foreach
+            // to find one to work with
+            // see WorkWithGroupRefactored below
             foreach (var group in GroupList)
             {
                 if (num == group.Number)
@@ -107,6 +136,8 @@ namespace OopPractice
                                       "'add' = add a student\n" +
                                       "'remove' = remove a student\n" +
                                       "'exit' = exit");
+                    // better to always 'normalize' user input by doing e.g. ToUpper().Trim() (like you did above)
+                    // to achieve better user experience
                     var command = Console.ReadLine();
 
                     do
@@ -115,6 +146,8 @@ namespace OopPractice
                         {
                             case "add":
                                 chosen.CreateAndAddStudent();
+                                // in every action Console.WriteLine("What next?"); is repeated so you can put it in the
+                                // end of the loop
                                 Console.WriteLine("What next?");
                                 break;
                             case "info":
@@ -143,6 +176,51 @@ namespace OopPractice
                         command = Console.ReadLine();
                     } while (command != "exit");
                 }
+            }
+        }
+
+        public static void WorkWithGroupRefactored(int groupNumber)
+        {
+            if (groupNumber >= GroupList.Count)
+            {
+                Console.WriteLine("There is no such group!");
+                return;
+            }
+            Console.WriteLine("What would you like to do?\n Class commands:\n" +
+                              "'info' = print out all the info on a group\n" +
+                              "'add' = add a student\n" +
+                              "'remove' = remove a student\n" +
+                              "'exit' = exit");
+            //  -1 because user will enter like group '1', but in our list (since it is 0-based) it is group 0
+            var chosenGroup = GroupList[groupNumber - 1];
+            var command = Console.ReadLine()!.ToUpper().Trim();;
+            while(command != "exit")
+            {
+                switch (command)
+                {
+                    case "add":
+                        chosenGroup.CreateAndAddStudent();
+                        break;
+                    case "info":
+                        chosenGroup.PrintInfo();
+                        break;
+                    case "remove":
+                        Console.WriteLine(
+                            "Please enter the name of the unfortunate sap you are trying to boot from your class:");
+                        var name = Console.ReadLine()!.Trim();
+                        chosenGroup.RemoveStudent(name);
+                        break;
+                    case "simulate":
+                        chosenGroup.SimulateResults();
+                        break;
+                    case "results":
+                        chosenGroup.OutputResults();
+                        break;
+                    case "exit":
+                        break;
+                }
+                Console.WriteLine("What next?");
+                command = Console.ReadLine()!.ToUpper().Trim();;
             }
         }
     }
