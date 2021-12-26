ca01d5552d447b227efad9bde2f546c4cfcf1d49
vladimir.nedoroslev@gmail.com <UT73G4bEwBiJcXG>
2021-12-27 01:32:13 +0600

    added comment to StringExtensions;

--- OopPracticeRefactored/Helpers/StringExtensions.cs
+++ OopPracticeRefactored/Helpers/StringExtensions.cs
@@ -2,6 +2,8 @@ namespace OopPracticeRefactored.Helpers
 {
     public static class StringExtensions
     {
+        // see
+        // https://docs.microsoft.com/en-us/dotnet/csharp/programming-guide/classes-and-structs/extension-methods
         public static string NormalizeInput(this string str)
         {
             return str.Trim().ToLower();
