d73e400b8ebe763c39e6a2d8dbce2690f11937d0
ShimadzuYo <55920277+ShimadzuYo@users.noreply.github.com>
2022-04-28 00:28:24 +0600

    Sibers task

---
+++ WebApp/Controllers/EmployeeController.cs
@@ -0,0 +1,29 @@
+using Application.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+using WebApp.Models;
+using WebApp.Models.Projects;
+
+namespace WebApp.Controllers;
+
+[Route("[controller]/[action]")]
+
+public class EmployeeController : Controller
+{
+    private readonly IEmployeeService _employeeService;
+
+    public EmployeeController(IEmployeeService employeeService)
+    {
+        _employeeService = employeeService;
+    }
+    
+    [HttpGet]
+    public IActionResult List()
+    {
+        var employees = _employeeService.GetEmployees();
+        var viewModel = new EmployeeListViewModel
+        {
+             Employees = employees
+        };
+        return View(viewModel);
+    }
+}
---
+++ WebApp/Models/EmployeeListViewModel.cs
@@ -0,0 +1,8 @@
+using Domain;
+
+namespace WebApp.Models;
+
+public class EmployeeListViewModel
+{
+    public List<Employee> Employees { get; set; }
+}
